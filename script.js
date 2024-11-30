676ef8b296c59258f44611d144fc678b3de336b3
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-30 14:36:07 +0300

    Add functionality to calculate numbers

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function subtract(a, b, operator) {
 }
 
 function multiply(a, b, operator) {
-  operator = 'x';
+  operator = '×';
   return a * b; 
 }
 
@@ -27,7 +27,7 @@ function operate(a, operator, b) {
       return add(a, b);
     case '-':
       return subtract(a, b);
-    case 'x':
+    case '×':
       return multiply(a, b);
     case '/':
       return divide(a, b);
@@ -48,3 +48,26 @@ for (let number of numberButtons) {
   })
 }
 
+let firstValue;
+let secondValue;
+let operatorSign;
+// Add all operator numbers
+const functionalButton = document.querySelectorAll('.functional-button');
+for (let operator of functionalButton) {
+  operator.addEventListener('click', (e) => {
+    firstValue = displayNumber;
+    display.textContent = ''; 
+    displayNumber = '';
+    operatorSign = operator.innerText;
+  })
+};
+// On click of operator store displayNumber as value1 
+// Store operator sign and clear display.textContent and displayNumber
+const equalsButton = document.querySelector('#equalsButton');
+equalsButton.addEventListener('click', () => {
+  secondValue = displayNumber;
+  const result = operate(+firstValue, operatorSign, +secondValue);
+  display.textContent = result;
+})
+// And on pressing equals assign displayNumber as value2 
+// And call operate(value1, operator, value2);
