8716d93a51669866451b79251261a7933b35aa26
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-30 11:06:18 +0300

    Create operate()

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function divide(a, b, operator) {
   operator = '/';
   return a / b; 
 }
+
+// Takes two numbers and an operator
+// Depending on what operator is passed
+// Calls appropriate function
+function operate(a, operator, b) {
+  switch(operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case 'x':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
