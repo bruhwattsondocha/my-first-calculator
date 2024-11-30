507415e5adc88f9fc6987853ea138da0cbb5e0ed
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-30 10:59:49 +0300

    Create add(), subtract(), multiply(), divide()

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b, operator) {
+  operator = '+';
+  return a + b; 
+};
+
+function subtract(a, b, operator) {
+  operator = '-';
+  return a - b; 
+}
+
+function multiply(a, b, operator) {
+  operator = 'x';
+  return a * b; 
+}
+
+function divide(a, b, operator) {
+  operator = '/';
+  return a / b; 
+}
