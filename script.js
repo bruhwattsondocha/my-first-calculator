a8ae70c6f80e5fa264b923815e6856c013c65dbb
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-12-01 12:57:35 +0300

    Prevent equals with no numbers
    Now it doesn't give NaN but just doesn't work if there are no numbers

--- script.js
+++ script.js
@@ -94,6 +94,7 @@ for (let operator of functionalButton) {
 
 // ON PRESSING EQUALS BUTTON
 equalsButton.addEventListener('click', () => {
+  if (!firstValue && !secondValue) return 1;
   if (!secondValue) {
    secondValue = tempValue;
   }
