ff8fad2ae247fd6d52e3191818db25976aca984a
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-12-01 18:52:13 +0300

    Bad commit did almost everything

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ functionalButtons.forEach(button => {
 // 
 // Do this how many times you want
 // After pressing EQUALS sign loop through array to find operator with higher precedence
+equalsButton.addEventListener('click', calculate);
 // FOR NOW they are MULTIPLY or DIVIDE and after them SUM and SUBTRACT
 // Take expressionArray[operatorIndex] and call a function that uses OPERATOR sign and operates expressionArray[operatorIndex - 1] and expressionArray[operatorIndex + 1] 
 // and store it in expressionArray[operatorIndex - 1] and remove expressionArray[operatorIndex] and expressionArray[operatorIndex + 1]
@@ -77,3 +78,63 @@ function appendToBuffer(number) {
 function refreshDisplay(number) {
   displayExpression.textContent += number;
 }
+function displayFinalResult(number) {
+  displayExpression.textContent = number;
+}
+
+function calculate() {
+  pushValue();
+  // operators: (× and /),(+ and -)
+  do {
+    // Find multiply or division operator index 
+    let operatorIndex = expressionArray.findIndex(item => item === '×' || item === '/');
+    // If operator is '×' call multiply function
+    if (expressionArray[operatorIndex] === '×') {
+      expressionArray[operatorIndex - 1] = multiply(+expressionArray[operatorIndex - 1], +expressionArray[operatorIndex + 1])
+      expressionArray.splice(operatorIndex, 2);
+    }
+    // If operator is '/' call divide function
+    else if ((expressionArray[operatorIndex] === '/')) {
+      expressionArray[operatorIndex - 1] = divide(+expressionArray[operatorIndex - 1], +expressionArray[operatorIndex + 1])
+      expressionArray.splice(operatorIndex, 2);
+    }
+    // If there are no more division and multiplication operators
+    if (expressionArray.findIndex(item => item === '×' || item === '/') === -1) {
+      // Find add and subtract operator index
+      operatorIndex = expressionArray.findIndex(item => item === '+' || item === '-');
+    }
+    // If operator is '+' call add function
+    if (expressionArray[operatorIndex] === '+') {
+      expressionArray[operatorIndex - 1] = add(+expressionArray[operatorIndex - 1], +expressionArray[operatorIndex + 1])
+      expressionArray.splice(operatorIndex, 2);
+    }
+    // If operator is '-' call subtract function
+    else if ((expressionArray[operatorIndex] === '-')) {
+      expressionArray[operatorIndex - 1] = subtract(+expressionArray[operatorIndex - 1], +expressionArray[operatorIndex + 1])
+      expressionArray.splice(operatorIndex, 2);
+    }
+  } 
+  while (expressionArray.length !== 1);
+  const result = expressionArray[0];
+  displayFinalResult(result);
+}
+
+// expressionArray.pop()
+// '/'
+// expressionArray
+// (5) ['2', '+', '2', '×', '2']
+// operatorIndex = expressionArray.findIndex(item => item === '×')
+// 3
+// expressionArray[operatorIndex - 1] = expressionArray[operatorIndex - 1] * expressionArray[operatorIndex + 1]
+// 4
+// expressionArray
+// (5) ['2', '+', 4, '×', '2']
+// expressionArray.splice(operatorIndex, 2)
+// (2) ['×', '2']
+// expressionArray
+// (3) ['2', '+', 4]
+// operatorIndex = expressionArray.findIndex(item => item === '+')
+// 1
+// expressionArray[operatorIndex - 1] = expressionArray[operatorIndex - 1] + expressionArray[operatorIndex + 1]
+// '24'
+// ﻿
